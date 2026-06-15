cd982986a9616e0671fc9e767d395d937df85de4
MarcosOliveiraJ <marcosoliveiradejesusjunior@gmail.com>
2026-06-15 17:10:35 -0300

    Criado controller

---
+++ Cadastro/src/main/java/com/suicobrasileiro/Cadastro/controllers/ControllerCadastro.java
@@ -0,0 +1,15 @@
+package com.suicobrasileiro.Cadastro.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class ControllerCadastro {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Benvindo, essa é minha primeira mensagem nesta rota";
+    }
+}
